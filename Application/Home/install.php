4971dce2162c21ca96647f338a9ec1db236711bd
Chishin <pro@xxgzs.org>
2016-09-29 19:58:19 +0800

    添加ThinkPHP核心库

---
+++ Application/Home/install.php
@@ -0,0 +1,352 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=2.0, user-scalable=yes" />
+    <title>小歆记账Web安装向导</title>
+    <style type="text/css">
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size:12px;
+        color:#666666;
+        background:#fff;
+        text-align:center;
+
+    }
+
+    * {
+        margin:0;
+        padding:0;
+    }
+
+    a {
+        color:#1E7ACE;
+        text-decoration:none;    
+    }
+
+    a:hover {
+        color:#000;
+        text-decoration:underline;
+    }
+    h3 {
+        font-size:14px;
+        font-weight:bold;
+    }
+
+    pre,p {
+        color:#1E7ACE;
+        margin:4px;
+    }
+    input, select,textarea {
+        padding:1px;
+        margin:2px;
+        font-size:11px;
+    }
+    .buttom{
+        padding:1px 10px;
+        font-size:12px;
+        border:1px #1E7ACE solid;
+        background:#D0F0FF;
+    }
+    #formwrapper {
+        width:350px;
+        margin:15px auto;
+        padding:20px;
+        text-align:left;
+        border:1px #1E7ACE solid;
+    }
+
+    fieldset {
+        padding:10px;
+        margin-top:5px;
+        border:1px solid #1E7ACE;
+        background:#fff;
+    }
+
+    fieldset legend {
+        color:#1E7ACE;
+        font-weight:bold;
+        padding:3px 20px 3px 20px;
+        border:1px solid #1E7ACE;    
+        background:#fff;
+    }
+
+    fieldset label {
+        float:left;
+        width:120px;
+        text-align:right;
+        padding:4px;
+        margin:1px;
+    }
+
+    fieldset div {
+        clear:left;
+        margin-bottom:2px;
+    }
+
+    .enter{ text-align:center;}
+    .clear {
+        clear:both;
+    }
+
+    </style>
+
+</head>
+<body>
+    <?php
+        if (file_exists('install.tmp')) {
+            ShowAlert('你已经安装过小歆记账，如需重装请删除根目录下的“install.tmp”文件!','无法安装');
+        } elseif (version_compare(PHP_VERSION,'5.3.0','<')) {
+            ShowAlert('PHP版本过低，请使用5.3及其以上版本！','无法安装');
+        } elseif (isset($_POST['submit'])) {
+            echo $_POST['submit'];
+            $Data = array();
+            $Data['db']['host'] = $_POST['db_host'];
+            $Data['db']['user'] = $_POST['db_user'];
+            $Data['db']['psw'] = $_POST['db_psw'];
+            $Data['db']['name'] = $_POST['db_name'];
+            $Data['db']['prefix'] = $_POST['db_prefix'];
+            $Data['mail']['smtp'] = $_POST['mail_smtp'];
+            $Data['mail']['user'] = $_POST['mail_user'];
+            $Data['mail']['psw'] = $_POST['mail_psw'];
+            $Data['mail']['from'] = $_POST['mail_from'];
+            $Data['admin']['user'] = $_POST['admin_user'];
+            $Data['admin']['psw'] = $_POST['admin_psw'];
+            $Data['admin']['email'] = $_POST['admin_email'];
+            CheakData($Data);
+            //InstallDB($Data['db'],$Data['admin']);
+            WriteConfig($Data['db'],$Data['mail']);
+            $jsonData = base64_encode(json_encode($Data));
+            $dbFile = fopen("install.tmp", "w") or die(ShowAlert("安装文件没有写入权限，请修改文件权限！","安装失败"));
+            fwrite($dbFile, $jsonData);
+            fclose($dbFile);
+            
+        } else {
+    ?>
+    <div id="formwrapper">
+        <h3 class="enter"><p>小歆记账Web安装向导</p></h3><br/>
+        <form action="install.php" method="post">   
+            <fieldset>
+                <legend>数据库配置</legend>
+                <div>
+                    <label>数据库主机</label>
+                    <input type="text" name="db_host" value="localhost">
+                </div>
+                <div>
+                    <label>数据库用户名</label>
+                    <input type="text" name="db_user" value="root">
+                </div>
+                <div>
+                    <label>数据库密码</label>
+                    <input type="password" name="db_psw" value="">
+                </div>
+                <div>
+                    <label>数据库名称</label>
+                    <input type="text" name="db_name" value="">
+                </div>
+                <div>
+                    <label>数据库表前缀</label>
+                    <input type="text" name="db_prefix" value="xxjz_">
+                </div>
+            </fieldset>
+                
+            <fieldset>
+                <legend>邮箱配置(可选)</legend>
+                <div>
+                    <label>邮箱SMTP主机</label>
+                    <input type="text" name="mail_smtp" value="smtp.163.com">
+                </div>
+                <div>
+                    <label>邮箱用户名</label>
+                    <input type="text" name="mail_user" value="">
+                </div>
+                <div>
+                    <label>邮箱密码</label>
+                    <input type="password" name="mail_psw" value="">
+                </div>
+                <div>
+                    <label>发件人邮箱</label>
+                    <input type="email" name="mail_from" value="xxjz@163.com">
+                </div>
+            </fieldset>
+
+            <fieldset>
+                <legend>管理员信息</legend>
+                <div>
+                    <label>管理员账号</label>
+                    <input type="text" name="admin_user" value="admin">
+                </div>
+                <div>
+                    <label>管理员密码</label>
+                    <input type="password" name="admin_psw" value="">
+                </div>
+                <div>
+                    <label>管理员邮箱</label>
+                    <input type="email" name="admin_email" value="xxjz@xxgzs.org">
+                </div>
+            </fieldset>
+            <br/>
+            <span style="display:block; text-align:center;">
+                <input type="submit" class="buttom" name="submit" value="安装" />   
+            </span>
+        </form> 
+    </div>
+    <?php
+        }
+    ?>
+</body>
+</html>
+
+<?php
+    function ShowAlert($Str,$Title="提示") {
+        echo '<div id="formwrapper"><fieldset>';
+        echo '<legend>'.$Title.'</legend><div style="text-align:center;">';
+        echo '<h3>'.$Str.'</h3><br/>';
+        echo '<a href="install.php">返回</a>';
+        echo '</div></fieldset></div>';
+    }
+
+    //验证数据
+    function CheakData($Data) {
+        if(strlen($Data['db']['host']) == 0) {
+            die(ShowAlert('数据库主机不能为空','安装失败'));
+        }
+        if(strlen($Data['db']['user']) == 0) {
+            die(ShowAlert('数据库用户名不能为空','安装失败'));
+        }
+        if(strlen($Data['db']['psw']) == 0) {
+            die(ShowAlert('数据库密码不能为空','安装失败'));
+        }
+        if(strlen($Data['db']['name']) == 0) {
+            die(ShowAlert('数据库表名不能为空','安装失败'));
+        }
+        if(strlen($Data['db']['prefix']) <= 1) {
+            die(ShowAlert('数据库表名前缀不能为空','安装失败'));
+        }
+        if(strlen($Data['admin']['user']) <= 1) {
+            die(ShowAlert('管理员账号无效，请重新输入。','安装失败'));
+        }
+        if(strlen($Data['admin']['psw']) < 6) {
+            die(ShowAlert('管理员密码太短，请重新输入。','安装失败'));
+        }
+    }
+
+    //数据库是否存在
+    function inDataBase($DbName,$Conn){
+        mysql_select_db("information_schema",$Conn);
+        $sql="select * from SCHEMATA where SCHEMA_NAME='".$DbName."'";
+        $query=mysql_query($sql);
+        $indb=is_array($row=mysql_fetch_array($query));
+        return $indb;
+    }
+    //表是否存在
+    function inTable($DbName,$TableName,$Conn){
+        mysql_select_db("information_schema",$Conn);
+        $sql="select * from TABLE_CONSTRAINTS where TABLE_SCHEMA='".$DbName."' and TABLE_NAME='".$TableName."'";
+        $query=mysql_query($sql);
+        $intable=is_array($row=mysql_fetch_array($query));
+        mysql_select_db($DbName,$Conn);//重新关联账本数据库
+        return $intable;
+    }
+
+    //安装数据库
+    function InstallDB($DbData, $DbUser) {
+        //连接数据库
+        $Conn = mysql_connect($DbData['host'],$DbData['user'],$DbData['psw']);
+        if (!$Conn) {
+            die(ShowAlert('请检查数据库主机地址、用户名和密码是否正确。','连接数据库失败'));
+        } else {
+            //创建数据库(已存在则跳过)
+            if(!inDataBase($DbData['name'],$Conn)){
+                $sql = "create database ".$DbData['name']." default character SET utf8 COLLATE utf8_general_ci";
+                $query=mysql_query($sql);
+                if(!$query){
+                    die(ShowAlert('请检查该用户是否有权限创建数据库。','创建数据库失败'));
+                }
+            }
+            //创建account表(已存在则报错)
+            $DbName = $DbData['name'];
+            $TableName = $DbData['prefix']."account";
+            if(intable($DbData['name'],$TableName,$Conn)){
+                die(ShowAlert('请删除数据库中的'.$TableName.'表，或者修改表前缀！','数据表已存在'));
+            }else{
+                $sql = "CREATE TABLE `$DbName`.`$TableName` (`acid` int(11) unsigned NOT NULL AUTO_INCREMENT,`acmoney` double(9,2) unsigned NOT NULL,`acclassid` int(8) NOT NULL,`actime` int(11) NOT NULL,`acremark` varchar(50) NOT NULL,`jiid` int(8) NOT NULL,`zhifu` int(8) NOT NULL,PRIMARY KEY (`acid`)) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;";
+                $query=mysql_query($sql);
+                if(!$query){
+                    die(ShowAlert('请检查该用户是否有权限创建表。','创建表失败'));
+                }
+            }
+            //创建account_class表(已存在则报错)
+            $TableName = $DbData['prefix']."account_class";
+            if(intable($DbData['name'],$TableName,$Conn)){
+                die(ShowAlert('请删除数据库中的'.$TableName.'表，或者修改表前缀！','数据表已存在'));
+            }else{
+                $sql = "CREATE TABLE `$DbName`.`$TableName` (`classid` int(5) NOT NULL AUTO_INCREMENT,`classname` varchar(20) NOT NULL,`classtype` int(1) NOT NULL,`ufid` int(11) NOT NULL,PRIMARY KEY (`classid`)) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;";
+                $query=mysql_query($sql);
+                if(!$query){
+                    die(ShowAlert('请检查该用户是否有权限创建表。','创建表失败'));
+                }
+            }
+            //创建user表
+            $TableName = $DbData['prefix']."user";
+            if(intable($DbData['name'],$TableName,$Conn)){
+                die(ShowAlert('请删除数据库中的'.$TableName.'表，或者修改表前缀！','数据表已存在'));
+            }else{
+                $sql = "CREATE TABLE `$DbName`.`$TableName` (`uid` int(11) NOT NULL AUTO_INCREMENT,`username` varchar(24) NOT NULL,`password` varchar(32) NOT NULL,`email` varchar(255) NOT NULL,`utime` int(11) NOT NULL,PRIMARY KEY (`uid`)) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;";
+                $query=mysql_query($sql);
+                if(!$query){
+                    die(ShowAlert('请检查该用户是否有权限创建表。','创建表失败'));
+                }
+            }
+            //创建管理员账号
+            $username = $DbUser['user'];
+            $password = md5($DbUser['psw']);
+            $email = $DbUser['email'];
+            $utime = strtotime("now");
+            $sql="select * from jizhang_user where username='$username'";
+            if(is_array(mysql_fetch_array(mysql_query($sql)))){
+                die(ShowAlert('默认用户已存在！','创建管理员失败'));
+            }else{
+                $utime=strtotime("now");
+                $sql="insert into jizhang_user (username, password,email,utime) values ('$username', '$password','$email','$utime')";
+                $query=mysql_query($sql);
+                if(!$query){
+                    die(ShowAlert('请检查该用户是否有权限添加数据权限。','创建管理员失败'));
+                }
+            }
+        }
+    }
+
+    //写入配置文件 => /Application/Common/Conf/config.php
+    function WriteConfig($DbData, $EmailData) {
+        $cFile = fopen("./Application/Common/Conf/config.php", "w") or die("Unable to open file!");
+        $txt = "<?php return array( //'配置项'=>'配置值' \n";
+        $txt = $txt."\n//数据库配置信息\n";
+        $txt = $txt."'DB_TYPE'   => 'mysql',  // 数据库类型 \n";
+        $txt = $txt."'DB_HOST'   => '". $DbData['host'] ."',       // 服务器地址 \n";
+        $txt = $txt."'DB_NAME'   => '". $DbData['name'] ."',       // 数据库名 \n";
+        $txt = $txt."'DB_USER'   => '". $DbData['user'] ."',       // 用户名 \n";
+        $txt = $txt."'DB_PWD'    => '". $DbData['psw']  ."',       // 密码 \n";
+        $txt = $txt."'DB_PORT'   => 3306,     // 端口 \n";
+        $txt = $txt."'DB_PARAMS' =>  array(), // 数据库连接参数 \n";
+        $txt = $txt."'DB_PREFIX' => '". $DbData['prefix'] ."',  // 数据库表前缀  \n";
+        $txt = $txt."'DB_CHARSET'=> 'utf8',   // 字符集 \n";
+        $txt = $txt."'DB_DEBUG'  =>  TRUE,    // 数据库调试模式 开启后可以记录SQL日志 \n";
+
+        $txt = $txt."\n//系统配置\n";
+        $txt = $txt."'HTML_CACHE_ON'         => false,       // 关闭静态缓存 \n";
+        $txt = $txt."'URL_CASE_INSENSITIVE'  => false,       // 区分大小写(必须) \n";
+
+        $txt = $txt."\n//应用配置信息\n";
+        $txt = $txt."'USER_LOGIN_TIMES'  => 10,              // 用户登录次 \n";
+        $txt = $txt."'PAGE_SIZE'         => 15,              // 表格分页数 \n";
+        $txt = $txt."'MAIL_HOST'         => '".$EmailData['smtp']."',              // 邮箱SMTP主机 \n";
+        $txt = $txt."'MAIL_USERNAME'     => '".$EmailData['user']."',              // 邮箱用户名 \n";
+        $txt = $txt."'MAIL_PASSWORD'     => '".$EmailData['psw'] ."',              // 邮箱密码 \n";
+        $txt = $txt."'MAIL_FROM'         => '".$EmailData['from']."',              // 发件人邮箱 \n";
+        $txt = $txt."'MAIL_FROMNAME'     => '小歆记账',      // 发件人名字 \n";
+
+        fwrite($cFile, $txt);
+        fclose($cFile);
+    }
+?>
