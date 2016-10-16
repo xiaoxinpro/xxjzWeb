53f77d5521cb271d62d7eaf1a00c2655fdd31610
Chishin <pro@xxgzs.org>
2016-10-16 13:47:02 +0800

    增加入口安装数据库检测

--- Application/Home/Controller/BaseController.class.php
+++ Application/Home/Controller/BaseController.class.php
@@ -4,6 +4,9 @@ use Think\Controller;
 class BaseController extends Controller {
     public function _initialize() {
         header('Content-type:text/html;charset=utf-8');
+        if (!C('DB_HOST')) {
+        	$this -> error('请先访问install.php进行安装！','./install.php');
+        }
         if(!UserShell(session('username'),session('user_shell'))) {
             $this -> redirect('Home/Login/index');
         }
