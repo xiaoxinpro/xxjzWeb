af6d380f5db40ba60254845e0488891087f73953
Chishin <pro@xxgzs.org>
2019-01-15 12:45:56 +0800

    创建资金账户编辑界面框架

--- Application/Home/Controller/FundsController.class.php
+++ Application/Home/Controller/FundsController.class.php
@@ -16,4 +16,37 @@ class FundsController extends BaseController {
         $this -> assign('ShowData', $FundsData);
         $this -> display();
     }
+
+    public function edit(){
+        $uid = session('uid');
+        $fundsid = I('get.id','',int);
+        if ($fundsid > 0) {
+            $DbFunds = GetFundsIdData($fundsid, $uid);
+            if (IS_POST) {
+                $fundsSubmit = I('post.funds_submit','');
+                if ($fundsSubmit === '编辑') {
+                    $fundsName = I('post.funds_name','');
+                    dump($fundsName);
+                    die();
+                } elseif ($fundsSubmit === '删除') {
+                    $fundsChange = I('post.funds_change','',int);
+                    dump($fundsChange);
+                    die();
+                } else {
+                    $this -> error('非法操作...');
+                }
+            } else {
+                if ($DbFunds[0]) {
+                    $this -> assign('FundsId', $fundsid);
+                    $this -> assign('FundsName', $DbFunds[1]['fundsname']);
+                    $this -> assign('FundsData', GetFundsData($uid));
+                    $this -> display();  
+                } else {
+                    $this -> error($DbData[1]);
+                }
+            }
+        } else {
+            $this -> error('非法操作...');
+        }
+    }
 }
