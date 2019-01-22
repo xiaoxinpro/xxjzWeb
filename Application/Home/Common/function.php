015f27b77f6c4937c0c8a5ffea80c6d4bbd4aa0b
Chishin <pro@xxgzs.org>
2019-01-22 13:02:52 +0800

    增加修改config的函数

--- Application/Home/Common/function.php
+++ Application/Home/Common/function.php
@@ -1295,4 +1295,41 @@
         return $rst;
     }
 
+    /**
+     * 修改config的函数
+     * @param $arr1 配置前缀
+     * @param $arr2 数据变量
+     * @return bool 返回状态
+     */
+    function setconfig($pat, $rep)
+    {
+        /**
+         * 原理就是 打开config配置文件 然后使用正则查找替换 然后在保存文件.
+         * 传递的参数为2个数组 前面的为配置 后面的为数值.  正则的匹配为单引号  如果你的是分号 请自行修改为分号
+         * $pat[0] = 参数前缀;  例:   default_return_type
+           $rep[0] = 要替换的内容;    例:  json
+         */
+        if (is_array($pat) and is_array($rep)) {
+            for ($i = 0; $i < count($pat); $i++) {
+                $pats[$i] = '/\'' . $pat[$i] . '\'(.*?),/';
+                if (is_array($rep[$i])) {
+                    $reps[$i] = "'". $pat[$i]. "'". " => array(" . implode(",", $rep[$i]) ."),";
+                } else if (is_string($rep[$i])) {
+                    $reps[$i] = "'". $pat[$i]. "'". " => " . "'".$rep[$i] ."',";
+                } elseif (is_numeric($rep[$i])) {
+                    $reps[$i] = "'". $pat[$i]. "'". " => " . strval($rep[$i]) .",";
+                } else {
+                    $reps[$i] = "'". $pat[$i]. "'". " => " . (($rep[$i] == true) ? "true" : "false") .",";
+                }
+            }
+            $fileurl = APP_PATH . "/Common/Conf/config.php";
+            $string = file_get_contents($fileurl); //加载配置文件
+            $string = preg_replace($pats, $reps, $string); // 正则查找然后替换
+            file_put_contents($fileurl, $string); // 写入配置文件
+            return true;
+        } else {
+            return flase;
+        }
+    }
+
 ?>
