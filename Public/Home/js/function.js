5ef6854480d796e9d10f35f1f27cad8dad713fe6
Chishin <pro@xxgzs.org>
2019-01-25 15:47:42 +0800

    增加数值格式化函数

--- Public/Home/js/function.js
+++ Public/Home/js/function.js
@@ -62,3 +62,29 @@ function ChangClass(s,sid,oid){
     obj.click();
 }
 
+//数值格式化函数(数值,保留几位小数,小数点符号,千分位符号)
+function number_format(number, decimals, dec_point, thousands_sep) {
+　　 number = (number + '').replace(/[^0-9+-Ee.]/g, '');
+　　 var n = !isFinite(+number) ? 0 : +number,
+　　 prec = !isFinite(+decimals) ? 2 : Math.abs(decimals),
+ 　　sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+ 　　dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+ 　　s = '',
+　　 toFixedFix = function(n, prec) {
+ 　　　　var k = Math.pow(10, prec);
+ 　　　　return '' + Math.ceil(n * k) / k;
+ 　　};
+
+ 　　s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+　　 var re = /(-?\d+)(\d{3})/;
+　　 while(re.test(s[0])) {
+ 　　　　s[0] = s[0].replace(re, "$1" + sep + "$2");
+ 　　}
+
+ 　　if((s[1] || '').length < prec) {
+    　　 s[1] = s[1] || '';
+     　　s[1] += new Array(prec - s[1].length + 1).join('0');
+ 　　}
+  　　　　return s.join(dec);
+ }
+
