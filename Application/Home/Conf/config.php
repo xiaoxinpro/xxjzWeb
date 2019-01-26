dda5d46a5b98a58a53f6763ec116d1e9bf7ecd66
Chishin <pro@xxgzs.org>
2019-01-26 09:08:03 +0800

    增加金额格式配置

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,7 +1,11 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
-	'APP_DEMO_USERNAME' => 'demo',
-	'APP_IOS_URL' => 'http://downloadpkg.apicloud.com/app/download?path=http://7xwwr4.com1.z0.glb.clouddn.com/3148046adbdb7b3ae9736fb35e5b26ae.ipa',
-	'APP_ANDROID_URL' => 'http://downloadpkg.apicloud.com/app/download?path=http://7xwwr4.com1.z0.glb.clouddn.com/dcace9ca9ecf952a679c88e04e7cf3bf_d'
+    //'配置项'=>'配置值'
+    'APP_DEMO_USERNAME' => 'demo',
+    'APP_IOS_URL' => 'http://downloadpkg.apicloud.com/app/download?path=http://7xwwr4.com1.z0.glb.clouddn.com/3148046adbdb7b3ae9736fb35e5b26ae.ipa',
+    'APP_ANDROID_URL' => 'http://downloadpkg.apicloud.com/app/download?path=http://7xwwr4.com1.z0.glb.clouddn.com/dcace9ca9ecf952a679c88e04e7cf3bf_d',
+
+    'MONEY_FORMAT_DECIMALS' => 2,    //金额保留小数位数（0-2）
+    'MONEY_FORMAT_POINT' => '.',     //金额小数点符号（.）
+    'MONEY_FORMAT_THOUSANDS' => ',', //金额千分符（,）
 );
