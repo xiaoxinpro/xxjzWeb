495fb194ec4c92d334a4dbe8c458eea7af6b4aff
chishin <pro@xxgzs.org>
2019-12-22 14:07:01 +0800

    修复Google翻译弹出需要翻译的bug

--- install.php
+++ install.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=2.0, user-scalable=yes" />
