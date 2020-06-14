37aeb47c3b48c5d10eaddba3e8b1329ca72b5490
chishin <pro@xxgzs.org>
2020-06-14 15:14:34 +0800

    更新版本号V2.1.2

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 定义APP版本号
-define('APP_VERSION', '2.1.1');
+define('APP_VERSION', '2.1.2');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
